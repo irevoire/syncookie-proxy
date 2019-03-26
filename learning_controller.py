6b6fc67ea53d5c17a5c99c757fdeb385fe7f0765
irevoire <irevoire@hotmail.fr>
2019-03-26 18:20:30 +0100

    remove the useless code in learning controller

--- learning_controller.py
+++ learning_controller.py
@@ -9,7 +9,6 @@ class CpuHeader(Packet):
     fields_desc = [BitField('macAddr',0,48), BitField('ingress_port', 0, 16)]
 
 class L2Controller(object):
-
     def __init__(self, sw_name):
 
         self.topo = Topology(db="topology.db")
@@ -21,19 +20,15 @@ class L2Controller(object):
         self.init()
 
     def init(self):
-
         self.controller.reset_state()
         self.add_boadcast_groups()
         self.add_mirror()
-        #self.fill_table_test()
 
     def add_mirror(self):
-
         if self.cpu_port:
             self.controller.mirroring_add(100, self.cpu_port)
 
     def add_boadcast_groups(self):
-
         interfaces_to_port = self.topo[self.sw_name]["interfaces_to_port"].copy()
         #filter lo and cpu port
         interfaces_to_port.pop('lo', None)
@@ -61,57 +56,13 @@ class L2Controller(object):
             mc_grp_id +=1
             rid +=1
 
-    def fill_table_test(self):
-        self.controller.table_add("dmac", "forward", ['00:00:0a:00:00:01'], ['1'])
-        self.controller.table_add("dmac", "forward", ['00:00:0a:00:00:02'], ['2'])
-        self.controller.table_add("dmac", "forward", ['00:00:0a:00:00:03'], ['3'])
-        self.controller.table_add("dmac", "forward", ['00:00:0a:00:00:04'], ['4'])
-
-
     def learn(self, learning_data):
-
         for mac_addr, ingress_port in  learning_data:
             print "mac: %012X ingress_port: %s " % (mac_addr, ingress_port)
             self.controller.table_add("smac", "NoAction", [str(mac_addr)])
             self.controller.table_add("dmac", "forward", [str(mac_addr)], [str(ingress_port)])
 
-    def unpack_digest(self, msg, num_samples):
-
-        digest = []
-        print len(msg), num_samples
-        starting_index = 32
-        for sample in range(num_samples):
-            mac0, mac1, ingress_port = struct.unpack(">LHH", msg[starting_index:starting_index+8])
-            starting_index +=8
-            mac_addr = (mac0 << 16) + mac1
-            digest.append((mac_addr, ingress_port))
-
-        return digest
-
-    def recv_msg_digest(self, msg):
-
-        topic, device_id, ctx_id, list_id, buffer_id, num = struct.unpack("<iQiiQi",
-                                                                          msg[:32])
-        digest = self.unpack_digest(msg, num)
-        self.learn(digest)
-
-        #Acknowledge digest
-        self.controller.client.bm_learning_ack_buffer(ctx_id, list_id, buffer_id)
-
-
-    def run_digest_loop(self):
-
-        sub = nnpy.Socket(nnpy.AF_SP, nnpy.SUB)
-        notifications_socket = self.controller.client.bm_mgmt_get_info().notifications_socket
-        sub.connect(notifications_socket)
-        sub.setsockopt(nnpy.SUB, nnpy.SUB_SUBSCRIBE, '')
-
-        while True:
-            msg = sub.recv()
-            self.recv_msg_digest(msg)
-
     def recv_msg_cpu(self, pkt):
-
         packet = Ether(str(pkt))
 
         if packet.type == 0x1234:
@@ -119,16 +70,11 @@ class L2Controller(object):
             self.learn([(cpu_header.macAddr, cpu_header.ingress_port)])
 
     def run_cpu_port_loop(self):
-
         cpu_port_intf = str(self.topo.get_cpu_port_intf(self.sw_name).replace("eth0", "eth1"))
         sniff(iface=cpu_port_intf, prn=self.recv_msg_cpu)
-
 
 if __name__ == "__main__":
     import sys
     sw_name = sys.argv[1]
-    receive_from = sys.argv[2]
-    if receive_from == "digest":
-        controller = L2Controller(sw_name).run_digest_loop()
-    elif receive_from == "cpu":
-        controller = L2Controller(sw_name).run_cpu_port_loop()
+    controller = L2Controller(sw_name).run_cpu_port_loop()
+
